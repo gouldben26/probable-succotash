3770c9a22a0a939abb84559475e3aa968532aa17
gouldben26 <80648332+gouldben26@users.noreply.github.com>
2025-12-09 07:25:33 -0500

    Update VladTheSimulator.ps1

--- VadimsScripts/PowerShell/VladTheSimulator.ps1
+++ VadimsScripts/PowerShell/VladTheSimulator.ps1
@@ -1,23 +1,6 @@
-Write-Host "Hi there! So you want to get into some shenanigans? Please select From one of the following options:
-Note: This script is intented to be ran on a host targeting a windows machine on the network
-1. Simiulate Port Scanning
-2. Simiulate Bloodhound Style Enumeration
-3. Simulate an AV Detection via EICAR. "
 
-$Option = Read-Host "Enter your choice"
-$TargetIP = Read-Host "Enter the target IP address"
-
-If ($Option -eq 1) {
-    Simulate-PortScanning($TargetIP)
-}
-ElseIf ($Option -eq 2) {
-    Simulate-BloodhoundStyleEnumeration($TargetIP)
-}
-ElseIf ($Option -eq 3) {
-    Simulate-AVDetectionViaEICAR($TargetIP)
-}
-
-function Simulate-PortScanning($TargetIP){
+function Simulate-PortScanning{
+    param($TargetIP)
     Write-Host "Performing Port Scanning on $TargetIP"
     $Port = 1..1024 
     ForEach ($Port in $Port) {
@@ -34,7 +17,8 @@ function Simulate-PortScanning($TargetIP){
     }
 }
 
-Function Simulate-BloodhoundStyleEnumeration {($TargetIP)
+Function Simulate-BloodhoundStyleEnumeration {
+    param($TargetIP)
     Write-Host "Performing Bloodhound Style Enumeration on $TargetIP via the IPC share"
     #SAMR Calls to enumerate local users and groups
     $Users = [ADSI]"WinNT://$TargetIP/Users"
@@ -57,12 +41,32 @@ Function Simulate-BloodhoundStyleEnumeration {($TargetIP)
     }
 }
 
-Function Simulate-AVDetectionViaEICAR {($TargetIP)
+Function Simulate-AVDetectionViaEICAR {
+    param($TargetIP)
     Write-Host "Performing AV Detection via EICAR on $TargetIP"
     New-PSsession -ComputerName $TargetIP
     Invoke-Command -ScriptBlock {{
         $EICARURL = "https://www.eicar.org/download/eicar.com.txt"
         Invoke-WebRequest -Uri $EICARURL -OutFile "C:\Windows\Temp\EICAR.txt"
     }}
+}
+
+Write-Host "Hi there! So you want to get into some shenanigans? Please select From one of the following options:
+Note: This script is intented to be ran on a host targeting a windows machine on the network
+1. Simiulate Port Scanning
+2. Simiulate Bloodhound Style Enumeration
+3. Simulate an AV Detection via EICAR. "
+
+$Option = Read-Host "Enter your choice"
+$TargetIP = Read-Host "Enter the target IP address"
+
+If ($Option -eq 1) {
+    Simulate-PortScanning($TargetIP)
+}
+ElseIf ($Option -eq 2) {
+    Simulate-BloodhoundStyleEnumeration($TargetIP)
+}
+ElseIf ($Option -eq 3) {
+    Simulate-AVDetectionViaEICAR($TargetIP)
 }
 
