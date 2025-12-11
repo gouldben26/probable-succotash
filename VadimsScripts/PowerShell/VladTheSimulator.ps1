ee4d0bc2adad7c56661e3e756213737c5c407295
gouldben26 <80648332+gouldben26@users.noreply.github.com>
2025-12-11 07:36:54 -0500

    Add files via upload

--- VadimsScripts/PowerShell/VladTheSimulator.ps1
+++ VadimsScripts/PowerShell/VladTheSimulator.ps1
@@ -1,8 +1,11 @@
 
 function Simulate-PortScanning{
-    param($TargetIP)
+    param(
+        [Parameter(Mandatory = $true)]
+        $TargetIP
+        )
     Write-Host "Performing Port Scanning on $TargetIP"
-    $Port = 1..1024 
+    $Port = 80,443,8080,8443,22,21,23,25,53,135,139,445,3389,389,53,636,3268,3269,464,593,88,137,138,139,445,389,53,636,3268,3269,464,593,88,137,138,139,445 
     ForEach ($Port in $Port) {
         $Socket = New-Object System.Net.Sockets.TcpClient
         Try {
@@ -18,38 +21,92 @@ function Simulate-PortScanning{
 }
 
 Function Simulate-BloodhoundStyleEnumeration {
-    param($TargetIP)
-    Write-Host "Performing Bloodhound Style Enumeration on $TargetIP via the IPC share"
+    param(
+        [Parameter(Mandatory = $true)]
+        $TargetIPCSV
+        )
+    
+    $TargetIPHosts = Get-HostsFromCSV $TargetIPCSV
+
+    for ($i = 0; $i -lt $TargetIPHosts.Count; $i++) {
+        Write-Host "Performing Bloodhound Style Enumeration on" $TargetIPHosts[$i] "via the IPC share"
     #SAMR Calls to enumerate local users and groups
+    $TargetIP = $TargetIPHosts[$i]
     $Users = [ADSI]"WinNT://$TargetIP/Users"
-    $Groups = [ADSI]"WinNT://$TargetIP/Groups"
-    $Users.Children | ForEach-Object {
-        Write-Host "User: $_.Name"
+    $Groups = [ADSI]"WinNT://$TragetIP/Groups"
+    Write-Host $Users
+    Write-Host "------------------------------"
+    Write-Host $Groups
+    #WKSVC Calls to enumerate local services on a remote host
+    $Services = Get-Service -ComputerName $TargetIP | Where-Object {$_.Status -eq "Running"}
+    Write-Host "-------------------------------"
+    Write-Host $Services
+    #SRVSVC Calls to enumerate sessions
+    Get-WmiObject -Class Win32_Session -ComputerName $TargetIP
     }
-    $Groups.Children | ForEach-Object {
-        Write-Host "Group: $_.Name"
-    }
-    #SRVSVC calls to enumerate services
-    $Services = [ADSI]"WinNT://$TargetIP/Services"
-    $Services.Children | ForEach-Object {
-        Write-Host "Service: $_.Name"
-    }
-    #WKSSVC call to enumerate workstation info 
-    $Workstation = [ADSI]"WinNT://$TargetIP/Workstation"
-    $Workstation.Children | ForEach-Object {
-        Write-Host "Workstation: $_.Name"
-    }
+    
+
+    
 }
 
 Function Simulate-AVDetectionViaEICAR {
-    param($TargetIP)
+    param(
+    [Parameter(Mandatory = $true)]    
+    $TargetIP
+    )
     Write-Host "Performing AV Detection via EICAR on $TargetIP"
-    New-PSsession -ComputerName $TargetIP
-    Invoke-Command -ScriptBlock {{
-        $EICARURL = "https://www.eicar.org/download/eicar.com.txt"
-        Invoke-WebRequest -Uri $EICARURL -OutFile "C:\Windows\Temp\EICAR.txt"
-    }}
+    $Credential = Get-Credential
+    Invoke-Command -ComputerName $TargetIP -Credential $Credential -ScriptBlock {
+        Invoke-WebRequest "https://www.eicar.org/download/eicar.com.txt" -o ./sample.txt
+    }
+    
 }
+
+Function Get-HostsFromCSV {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$CSVPath
+    )
+    if (-Not (Test-Path $CSVPath)) {
+        Write-Host "File $CSVPath does not exist."
+        return $null
+    }
+    $dataArray = @()
+    try {
+        $dataArray = Import-Csv -Path $CSVPath
+        Write-Host "Successfully imported $($dataArray.Count) rows from $CSVPath"
+    }
+    catch {
+        Write-Host "Failed to import CSV: $_"
+        return $null
+    }
+    return $HostArray
+}
+
+Write-Host "                   .---------------------------------------------------.
+                              /                                                     \
+                             /     /\                     /\                         \
+                            /     /  \                   /  \                         \
+                           /     /^^^^\_________________/^^^^\                         \
+                          /     /      \               /      \                         \
+                  /\     /     /  /\    \             /    /\  \     /\                 \
+                 /  \   /     /  /  \    \___________/    /  \  \   /  \                 \
+                /^^^^\_/_____/^^/^^^^\                 /^^^^\^^\___/^^^^\                |
+               /      \      /  /      \_______________/      \  \       \               |
+        ______/  /\    \    /  /        |             |        \  \    /\ \_______       |
+       |      \ /  \    \__/  /         |  /\     /\  |         \  \__/  \ /      |      |
+       | ^^^^^ /^^^^\        /   _      | |  |   |  | |      _    \        /^^^^^ |      |
+       |       \      \_____/   | |     | |  |___|  | |     | |    \______/       |      |
+       |        \    /\         |_|     | |   ___   | |     |_|         /\        |      |
+       |_________\  /  \________________| |  (___)  | |________________/  \_______|      |
+               /^^^^^^\                 |_|_________|_|                /^^^^^^\          |
+              /        \______________________________________________/        \         |
+             /                                                                 \        /
+            /___________________________________________________________________\______/
+
+
+                         ███████ Vlad the Simulator ███████
+"
 
 Write-Host "Hi there! So you want to get into some shenanigans? Please select From one of the following options:
 Note: This script is intented to be ran on a host targeting a windows machine on the network
@@ -64,9 +121,8 @@ If ($Option -eq 1) {
     Simulate-PortScanning($TargetIP)
 }
 ElseIf ($Option -eq 2) {
-    Simulate-BloodhoundStyleEnumeration($TargetIP)
+    Simulate-BloodhoundStyleEnumeration($TargetIPRange)
 }
 ElseIf ($Option -eq 3) {
     Simulate-AVDetectionViaEICAR($TargetIP)
 }
-
